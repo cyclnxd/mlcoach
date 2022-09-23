5c8b7ca04266c5de1ec855b8aebc5e85eeb3ecde
ShogunSkinoski <whitebirdsdk@gmail.com>
2022-09-23 12:29:45 +0300

    Static Node v1 and simpleImputation class will be refactored

--- components/Nodes/MissingValueNodes/fillWithStatsNode/index.js
+++ components/Nodes/MissingValueNodes/fillWithStatsNode/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, memo, useRef } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import store from "../../../../store/store.ts";
+import store from "../../../../lib/store/store.ts";
 import { Handle } from "react-flow-renderer";
 import {
   Card,
@@ -10,11 +10,13 @@ import {
   Autocomplete,
   Button,
   Pagination,
+  FormControl,
 } from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import SimpleImputation from "../../../../lib/utils/simpleImputation";
 
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
@@ -27,12 +29,15 @@ function FillWithStatsNode({ id, selected, data }) {
 
   const [error, setError] = useState("connect a data source to select columns");
 
-  const keys = ["Minumum", "Maximum", "Mean", "Median"];
+  const keys = ["Minimum", "Maximum", "Mean", "Median"];
   const [selectedKey, setSelectedKey] = useState("Mean");
 
   const [startImputation, setStartImputation] = useState("true");
-
   const [fileChanged, setFileChanged] = useState(false);
+  const [page, setPage] = useState(1);
+  const handlePagination = (event, Page) => {
+    setPage(Page);
+  };
 
   const handleButtonClick = (event) => {
     let value = startImputation !== "true" ? "true" : "false";
@@ -47,6 +52,12 @@ function FillWithStatsNode({ id, selected, data }) {
     prevSelectedColumn.current = value;
     setSelectedColumn(prevSelectedColumn.current);
   };
+
+  const [maximum, setMaximum] = useState(-1);
+  const [minimum, setMinimum] = useState(-1);
+  const [mean, setMean] = useState(-1);
+  const [median, setMedian] = useState(-1);
+
   useEffect(() => {
     // checking if the user has created a valid edge between two nodes
     const edge = Object.values(store.getState().edges).find(
@@ -74,89 +85,73 @@ function FillWithStatsNode({ id, selected, data }) {
         }
         setColumns([...set]);
         setSelectedColumn(prevSelectedColumn.current);
-        let mean = [];
-        let minumum = [];
-        let maximum = [];
-        let median = [];
-        let sum = 0;
-        switch (selectedKey) {
-          case "Median": {
-            if (selectedColumn.length !== 0) {
-              for (const column of selectedColumn) {
-                var array = []
-                for(var index in file.data){
-                  if(file.data[index][column]){
-                    array.push(file.data[index][column])
-                  }
-                }
-                 
-                array = quickSort(array);
-                median[column] = array[Math.floor(array.length / 2)];
-                for(var index in file.data){
-                  if(!file.data[index][column]){
-                    file.data[index][column] = median[column]
+        if (page === 1) {
+          switch (selectedKey) {
+            case "Median":
+              {
+                if (selectedColumn !== null) {
+                  for (const column of selectedColumn) {
+                    let imputation = new SimpleImputation("Median");
+                    imputation.transformData(column, file.data);
                   }
                 }
               }
-            }
-          }
-          case "Mean": {
-            if (selectedColumn.length !== 0) {
-              for (const column of selectedColumn) {
-                for (var index in file.data) {
-                  if (file.data[index][column]) {
-                    sum += parseInt(file.data[index][column]);
-                  }
-                }
-                mean[column] = sum / file.data.length;
-                for (var index in file.data) {
-                  if (!file.data[index][column]) {
-                    file.data[index][column] = mean[column];
+              break;
+
+            case "Mean":
+              {
+                if (selectedColumn !== null) {
+                  for (const column of selectedColumn) {
+                    let imputation = new SimpleImputation("Mean");
+                    imputation.transformData(column, file.data);
                   }
                 }
               }
-            }
-          }
-          case "Maximum": {
-            if (selectedColumn.length !== 0) {
-              let max = Number.MIN_VALUE;
-              for (const column of selectedColumn) {
-                max = Number.MIN_VALUE;
-                for (var index in file.data) {
-                  if (parseInt(file.data[index][column]) > max) {
-                    max = parseInt(file.data[index][column]);
-                  }
-                }
-                maximum[column] = max;
-                for (var index in file.data) {
-                  if (!file.data[index][column]) {
-                    file.data[index][column] = maximum[column];
+              break;
+
+            case "Maximum":
+              {
+                if (selectedColumn !== null) {
+                  for (const column of selectedColumn) {
+                    let imputation = new SimpleImputation("Maximum");
+                    imputation.transformData(column, file.data);
                   }
                 }
               }
-            }
+              break;
+            case "Minimum":
+              {
+                if (selectedColumn !== null) {
+                  let imputation = new SimpleImputation("Minimum");
+                  for (const column of selectedColumn) {
+                    imputation.transformData(column, file.data);
+                  }
+                }
+              }
+              break;
+            default:
           }
-          case "Minumum": {
-            let min = Number.MAX_VALUE;
-            if (selectedColumn.length !== 0) {
-              for (const column of selectedColumn) {
-                min = Number.MAX_VALUE;
-                for (var index in file.data) {
-                  if (parseInt(file.data[index][column]) < min) {
-                    min = parseInt(file.data[index][column]);
-                  }
-                }
-                minumum[column] = min;
-                for (var index in file.data) {
-                  if (!file.data[index][column]) {
-                    file.data[index][column] = minumum[column];
-                  }
-                }
+        } else {
+          if (selectedColumn !== null) {
+            for (const key of keys) {
+              let imputation = new SimpleImputation(key);
+              imputation.transformData(selectedColumn, file.data);
+              if (key === "Maximum") {
+                console.log(Number.MIN_VALUE);
+                setMaximum(imputation.getMaximum());
+              }
+              else if (key === "Minimum"){
+                setMinimum(imputation.getMinimum());
+              }
+              else if(key === "Mean"){
+                setMean(imputation.getMean());
+              }
+              else if(key === "Median"){
+                setMedian(imputation.getMedian());
               }
             }
           }
         }
-
         startImputation === "true"
           ? store.getState().storeFile(id, file)
           : startImputation === "true"
@@ -169,8 +164,165 @@ function FillWithStatsNode({ id, selected, data }) {
     } else {
       setError("connect a data source to slice data");
     }
-  }, [id, data, selectedColumn, selectedKey, startImputation, fileChanged]);
+  }, [
+    id,
+    data,
+    selectedColumn,
+    selectedKey,
+    startImputation,
+    fileChanged,
+    page,
+  ]);
 
+  function mainBody(page) {
+    if (page === 1) {
+      return (
+        <>
+          <Typography fontSize={11} sx={{ color: "primary.contrastText" }}>
+            Fill data with selected mode
+          </Typography>
+
+          <Autocomplete
+            multiple
+            id="tags-columns"
+            options={columns}
+            disableCloseOnSelect
+            getOptionLabel={(columns) => columns}
+            onChange={(event, value) => handleColumnChange(event, value)}
+            renderOption={(props, columns, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {columns}
+              </li>
+            )}
+            style={{ width: "170px", color: "primary.light" }}
+            renderInput={(params) => (
+              <TextField
+                sx={{ color: "primary.light" }}
+                {...params}
+                label="Column Box"
+                placeholder="Columns"
+              />
+            )}
+          />
+
+          <Autocomplete
+            id="tags-statistic"
+            options={keys}
+            getOptionLabel={(keys) => keys}
+            onChange={(event, value) => handleKeyChange(event, value)}
+            style={{ width: "170px", color: "primary.light" }}
+            renderOption={(props, columns, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {columns}
+              </li>
+            )}
+            renderInput={(params) => (
+              <TextField
+                sx={{ color: "primary.light" }}
+                {...params}
+                label="Variables Box"
+                placeholder="Columns"
+              />
+            )}
+          />
+          <Button
+            variant="contained"
+            value={startImputation}
+            onClick={() => handleButtonClick()}
+            sx={{
+              color: "primary.contrastText",
+              overflow: "hidden",
+              fontWeight: "300",
+              fontSize: "10px",
+              borderRadius: "0px!important",
+              backgroundColor: "primary.light",
+              "&.Mui-selected": {
+                color: "primary.darkText",
+                borderColor: "primary.light !important",
+              },
+            }}
+          >
+            Imput Selected Columns
+          </Button>
+          <Pagination
+            page={page}
+            onChange={handlePagination}
+            count={2}
+            variant="outlined"
+            shape="rounded"
+          />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Autocomplete
+            id="tags-columns"
+            options={columns}
+            disableCloseOnSelect
+            getOptionLabel={(columns) => columns}
+            onChange={(event, value) => handleColumnChange(event, value)}
+            renderOption={(props, columns, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {columns}
+              </li>
+            )}
+            style={{ width: "170px", color: "primary.light" }}
+            renderInput={(params) => (
+              <TextField
+                sx={{ color: "primary.light" }}
+                {...params}
+                label="Column Box"
+                placeholder="Columns"
+              />
+            )}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "left",
+              alignItems: "left",
+              pl: "0px",
+              gap: "5px",
+            }}
+          >
+            <Typography>Mean {mean !== Infinity ? mean.toFixed(2) : 0} </Typography>
+            <Typography>Median {median !== 1.7976931348623157e308 ? median : 0}</Typography>
+            <Typography>
+              Maximum {maximum !== 1.7976931348623157e308 ? maximum : 0}
+            </Typography>
+            <Typography>Minimum {minimum !== 1.7976931348623157e308 ? minimum : 0}</Typography>
+          </Box>
+          <Pagination
+            page={page}
+            onChange={handlePagination}
+            count={2}
+            variant="outlined"
+            shape="rounded"
+          />
+        </>
+      );
+    }
+  }
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       <meta name="viewport" content="width=device-width"></meta>
@@ -203,8 +355,6 @@ function FillWithStatsNode({ id, selected, data }) {
         sx={{
           backgroundColor: "primary.surface",
           color: "white",
-          justifyContent: "center",
-          alignItems: "center",
         }}
         style={
           selected
@@ -218,11 +368,9 @@ function FillWithStatsNode({ id, selected, data }) {
             sx={{
               display: "flex",
               flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
               padding: "10px",
-              gap: "15px",
-              width: "190px",
+              gap: "10px",
+              width: "200px",
               ".MuiOutlinedInput-root": {
                 color: "primary.contrastText",
                 fontSize: "12px",
@@ -251,90 +399,7 @@ function FillWithStatsNode({ id, selected, data }) {
             }}
           >
             {error.length === 0 ? (
-              <>
-                <Typography
-                  fontSize={11}
-                  sx={{ color: "primary.contrastText" }}
-                >
-                  Fill data with selected mode
-                </Typography>
-
-                <Autocomplete
-                  multiple
-                  id="tags-columns"
-                  options={columns}
-                  disableCloseOnSelect
-                  getOptionLabel={(columns) => columns}
-                  onChange={(event, value) => handleColumnChange(event, value)}
-                  renderOption={(props, columns, { selected }) => (
-                    <li {...props}>
-                      <Checkbox
-                        icon={icon}
-                        checkedIcon={checkedIcon}
-                        style={{ marginRight: 8 }}
-                        checked={selected}
-                      />
-                      {columns}
-                    </li>
-                  )}
-                  style={{ width: "170px", color: "primary.light" }}
-                  renderInput={(params) => (
-                    <TextField
-                      sx={{ color: "primary.light" }}
-                      {...params}
-                      label="Column Box"
-                      placeholder="Columns"
-                    />
-                  )}
-                />
-
-                <Autocomplete
-                  id="tags-statistic"
-                  options={keys}
-                  getOptionLabel={(keys) => keys}
-                  onChange={(event, value) => handleKeyChange(event, value)}
-                  style={{ width: "170px", color: "primary.light" }}
-                  renderOption={(props, columns, { selected }) => (
-                    <li {...props}>
-                      <Checkbox
-                        icon={icon}
-                        checkedIcon={checkedIcon}
-                        style={{ marginRight: 8 }}
-                        checked={selected}
-                      />
-                      {columns}
-                    </li>
-                  )}
-                  renderInput={(params) => (
-                    <TextField
-                      sx={{ color: "primary.light" }}
-                      {...params}
-                      label="Variables Box"
-                      placeholder="Columns"
-                    />
-                  )}
-                />
-                <Button
-                  variant="contained"
-                  value={startImputation}
-                  onClick={() => handleButtonClick(event)}
-                  sx={{
-                    color: "primary.contrastText",
-                    overflow: "hidden",
-                    fontWeight: "300",
-                    fontSize: "10px",
-                    borderRadius: "0px!important",
-                    backgroundColor: "primary.light",
-                    "&.Mui-selected": {
-                      color: "primary.darkText",
-                      borderColor: "primary.light !important",
-                    },
-                  }}
-                >
-                  Imput Selected Columns
-                </Button>
-                <Pagination count={2} variant="outlined" shape="rounded" />
-              </>
+              mainBody(page)
             ) : (
               <Typography>{error}</Typography>
             )}
@@ -369,25 +434,4 @@ function FillWithStatsNode({ id, selected, data }) {
     </Grid>
   );
 }
-
-function quickSort(origArray) {
-  if (origArray.length <= 1) {
-    return origArray;
-  } else {
-    var left = [];
-    var right = [];
-    var newArray = [];
-    var pivot = origArray.pop();
-    var length = origArray.length;
-    for (var i = 0; i < length; i++) {
-      if (origArray[i] <= pivot) {
-        left.push(origArray[i]);
-      } else {
-        right.push(origArray[i]);
-      }
-    }
-    return newArray.concat(quickSort(left), pivot, quickSort(right));
-  }
-}
-
 export default memo(FillWithStatsNode);
