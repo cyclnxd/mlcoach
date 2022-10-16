7c4413ea3ffcb39ea92f7098331a5d2c71dcc431
cyclnxd <herakles13579@gmail.com>
2022-10-16 14:43:58 +0300

    added start stop on editor

--- components/Editor/Flow/index.js
+++ components/Editor/Flow/index.js
@@ -10,14 +10,17 @@ import useDataStore from 'lib/store/DataStore.ts'
 import ToolModal from 'components/Editor/ToolPanel/components/ToolModal'
 import ConnectionLine from 'components/Editor/ConnectionLine'
 import { Box, Stack } from '@mui/system'
-import { Alert, Button } from '@mui/material'
+import { Button, ToggleButton, ToggleButtonGroup } from '@mui/material'
 import CustomDialog from 'components/Editor/Dialog'
 import { v4 as uuidv4 } from 'uuid'
 import ButtonMenu from 'components/Editor/ButtonMenu'
 import SaveIcon from '@mui/icons-material/Save'
 import FileOpenIcon from '@mui/icons-material/FileOpen'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import StopIcon from '@mui/icons-material/Stop'
 import html2canvas from 'html2canvas'
 import { useTranslations } from 'next-intl'
+import CustomAlert from 'components/base/CustomAlert'
 
 const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 	const t = useTranslations('editor')
@@ -28,6 +31,7 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 	const [openOpenMenu, setOpenOpenMenu] = useState(false)
 	const [work, setWork] = useState(null)
 	const [anchorEl, setAnchorEl] = useState(null)
+	const [mode, setMode] = useState('stop')
 
 	const editorRef = useRef(null)
 
@@ -37,20 +41,16 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 	const handleClose = () => {
 		setAnchorEl(null)
 	}
-	const handleOpenSaveMenu = () => {
-		setOpenSaveMenu(true)
+	const handleSaveMenu = () => {
+		setOpenSaveMenu(!openSaveMenu)
 	}
-	const handleOpenOpenMenu = () => {
-		setOpenOpenMenu(true)
-	}
-
-	const handleCloseSaveMenu = () => {
-		setOpenSaveMenu(false)
-	}
-	const handleCloseOpenMenu = () => {
-		setOpenOpenMenu(false)
+	const handleOpenMenu = () => {
+		setOpenOpenMenu(!openOpenMenu)
 	}
 
+	const handleMode = (_, change) => {
+		setMode(change || mode)
+	}
 	const {
 		nodes,
 		edges,
@@ -64,7 +64,9 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 		onEdgesDelete,
 		handleModal,
 		modalOpen,
+		calculate,
 	} = store(state => state)
+
 	const user = useAuthStore(state => state.user)
 	const {
 		updateWorkByUsername,
@@ -77,6 +79,12 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 		e.preventDefault()
 		handleModal(true)
 	}
+
+	useEffect(() => {
+		if (mode === 'start') {
+			calculate()
+		}
+	}, [calculate, mode])
 
 	const takeScreenshot = useCallback(
 		async id => {
@@ -118,7 +126,7 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 						setLoading(true)
 						if (work && work.name === name) {
 							await takeScreenshot(work.id)
-							const res = await getPublicUrl(work.id, 'thumbnails')
+							const res = getPublicUrl(work.id, 'thumbnails')
 							await updateWorkByUsername({
 								...work,
 								name,
@@ -130,7 +138,7 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 						} else {
 							const randomId = uuidv4()
 							await takeScreenshot(randomId)
-							const res = await getPublicUrl(randomId, 'thumbnails')
+							const res = getPublicUrl(randomId, 'thumbnails')
 							const work = await updateWorkByUsername({
 								id: randomId,
 								username: user?.username,
@@ -237,93 +245,155 @@ const Flow = ({ handleDeleteLog, handleDeleteGraph }) => {
 						direction='row'
 						spacing={2}
 						sx={{
+							width: '100%',
+							display: 'flex',
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							px: 1,
 							position: 'absolute',
 							top: '15px',
-							left: '15px',
 							zIndex: 1000,
 						}}>
-						<ButtonMenu
-							disabled={loading || !user}
-							menuDisabled={nodes.length === 0}
-							anchorEl={anchorEl}
-							handleClose={handleClose}
-							handleOpen={handleOpen}
-							title={t('buttons.editor')}
-							options={[
-								{
-									label: t('buttonMenu.save'),
-									icon: <SaveIcon />,
-									onClick: () => handleOpenSaveMenu(),
-								},
-								{
-									label: t('buttonMenu.open'),
-									icon: <FileOpenIcon />,
-									onClick: () => handleOpenOpenMenu(),
-								},
-							]}
-						/>
-						<CustomDialog
-							open={openOpenMenu}
-							handleClose={handleCloseOpenMenu}
-							callback={handleOpenEditor}
-							title={t('editorDialog.open')}
-							content={t('editorDialog.desc1')}
-							label={t('editorDialog.placeholder1')}
-							username={user?.username}
-						/>
-						<CustomDialog
-							open={openSaveMenu}
-							handleClose={handleCloseSaveMenu}
-							callback={handleSaveEditor}
-							title={t('editorDialog.save')}
-							content={t('editorDialog.desc2')}
-							label={t('editorDialog.placeholder1')}
-							username={user?.username}
-						/>
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'row',
+								gap: 2,
+								alignItems: 'center',
+							}}>
+							<ButtonMenu
+								disabled={loading || !user}
+								menuDisabled={nodes.length === 0}
+								anchorEl={anchorEl}
+								handleClose={handleClose}
+								handleOpen={handleOpen}
+								title={t('buttons.editor')}
+								options={[
+									{
+										label: t('buttonMenu.save'),
+										icon: <SaveIcon />,
+										onClick: () => handleSaveMenu(),
+									},
+									{
+										label: t('buttonMenu.open'),
+										icon: <FileOpenIcon />,
+										onClick: () => handleOpenMenu(),
+									},
+								]}
+							/>
+							<CustomDialog
+								open={openOpenMenu}
+								handleClose={handleOpenMenu}
+								callback={handleOpenEditor}
+								title={t('editorDialog.open')}
+								content={t('editorDialog.desc1')}
+								label={t('editorDialog.placeholder1')}
+								username={user?.username}
+							/>
+							<CustomDialog
+								open={openSaveMenu}
+								handleClose={handleSaveMenu}
+								callback={handleSaveEditor}
+								title={t('editorDialog.save')}
+								content={t('editorDialog.desc2')}
+								label={t('editorDialog.placeholder1')}
+								username={user?.username}
+							/>
 
-						<Button
-							variant='contained'
-							sx={{
-								backgroundColor: 'primary.darkLight',
-								color: 'primary.contrastText',
-							}}
+							<Button
+								variant='contained'
+								sx={{
+									backgroundColor: 'primary.darkLight',
+									color: 'primary.contrastText',
+								}}
+								size='small'
+								onClick={handleDeleteLog}>
+								{t('buttons.output')}
+							</Button>
+							<Button
+								variant='contained'
+								sx={{
+									backgroundColor: 'primary.darkLight',
+									color: 'primary.contrastText',
+								}}
+								size='small'
+								onClick={handleDeleteGraph}>
+								{t('buttons.graph')}
+							</Button>
+							<Button
+								variant='contained'
+								sx={{
+									backgroundColor: 'primary.darkLight',
+									color: 'primary.contrastText',
+								}}
+								size='small'
+								onClick={() => handleModal(true)}>
+								{t('buttons.toolbox')}
+							</Button>
+						</Box>
+
+						<ToggleButtonGroup
 							size='small'
-							onClick={handleDeleteLog}>
-							{t('buttons.output')}
-						</Button>
-						<Button
-							variant='contained'
-							sx={{
-								backgroundColor: 'primary.darkLight',
-								color: 'primary.contrastText',
-							}}
-							size='small'
-							onClick={handleDeleteGraph}>
-							{t('buttons.graph')}
-						</Button>
-						<Button
-							variant='contained'
-							sx={{
-								backgroundColor: 'primary.darkLight',
-								color: 'primary.contrastText',
-							}}
-							size='small'
-							onClick={() => handleModal(true)}>
-							{t('buttons.toolbox')}
-						</Button>
+							exclusive
+							aria-label='control'
+							value={mode}
+							onChange={handleMode}>
+							<ToggleButton
+								value={'start'}
+								aria-label='start'
+								sx={{
+									color: 'primary.darkText',
+									backgroundColor: 'primary.darkLight',
+									'&.Mui-selected': {
+										color: 'green',
+										backgroundColor: 'primary.light',
+									},
+									'&:hover': {
+										backgroundColor: 'primary.light',
+										color: 'primary.darkText',
+									},
+									'&.Mui-selected:hover': {
+										backgroundColor: 'primary.light',
+										color: 'green',
+									},
+								}}>
+								<PlayArrowIcon />
+							</ToggleButton>
+							<ToggleButton
+								value={'stop'}
+								aria-label='stop'
+								sx={{
+									color: 'primary.darkText',
+									backgroundColor: 'primary.darkLight',
+									'&.Mui-selected': {
+										color: 'red',
+										backgroundColor: 'primary.light',
+									},
+									'&:hover': {
+										backgroundColor: 'primary.light',
+										color: 'primary.darkText',
+									},
+									'&.Mui-selected:hover': {
+										backgroundColor: 'primary.light',
+										color: 'red',
+									},
+								}}>
+								<StopIcon />
+							</ToggleButton>
+						</ToggleButtonGroup>
 					</Stack>
-					{error ? (
-						<Alert
+					{error && (
+						<CustomAlert
 							severity='error'
+							message={error}
 							sx={{
 								position: 'absolute',
 								right: 10,
-								top: 10,
+								top: 60,
 								zIndex: 1000,
-							}}>
-							{error}
-						</Alert>
-					) : null}
+							}}
+						/>
+					)}
 					<Controls />
 
 					<ToolModal open={modalOpen} handleModal={handleModal} />
