b5faef1125e970150a9b792af2658e45bd1bbaa6
cyclnxd <herakles13579@gmail.com>
2022-10-16 14:41:46 +0300

    changed file struct

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 import numpy as np
+import os
 import joblib
 from pydantic.main import BaseModel
 
@@ -19,4 +20,4 @@ def predict(data: Model):
   return str(prediction)
 
 if __name__ == '__main__':
-  uvicorn.run(app, host='0.0.0.0', port=5000)
+  uvicorn.run(app, host='127.0.0.1', port=int(os.environ.get("PORT", 8080)))
